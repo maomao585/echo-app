94d04789b5a03ae40b1c8da7d71a33f92a071517
maomao <3323972328@qq.com>
2025-10-20 15:28:45 +0800

    feat: add basic echo app

---
+++ echo.py
@@ -0,0 +1,9 @@
+import sys
+
+def echo():
+    shout = "\\~s" in sys.argv  # 修正了转义字符
+    message = input("Enter something: ")
+    print(message.upper() if shout else message)
+
+if __name__ == "__main__":  # 修正了缩进
+    echo()
