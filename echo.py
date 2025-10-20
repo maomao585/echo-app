510a720c517729ad3b95d13bdcdb7645564e44a5
maomao <3323972328@qq.com>
2025-10-20 15:58:20 +0800

    refactor: alternative prompt on conflict branch

--- echo.py
+++ echo.py
@@ -2,7 +2,7 @@ import sys
 
 def echo():
     shout = "\\~s" in sys.argv  # 修正了转义字符
-    message = input("Enter something: ")
+    message = input("Type anything: ")
     print(message.upper() if shout else message)
 
 if __name__ == "__main__":  # 修正了缩进
